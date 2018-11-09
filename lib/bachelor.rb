e618bd1352c04b5d2f7b0777557c0eb2c15dd557
John Martinez <jmartinez729@gmail.com>
2018-11-09 17:49:31 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
+# def get_first_name_of_season_winner(data, season)
+#   data[season].each do |season, season_array|
+#     season_array.each do |contestant_information|
+#       if contestant_information[:status] == "Winner"
+#         return contestant_information[:name].split(" ")[0]
+#       end
+#     end
+#   end
+# end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants|
+    if contestants["status"] == "Winner"
+      return contestants["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-end
+  data.each do |season, season_array|
+    season_array.each do |contestant_information|
+      if contestant_information["occupation"] == occupation
+        return contestant_information["name"]
+      end
+    end 
+  end 
+end 
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, season_array|
+    season_array.each do |contestant_information|
+      if contestant_information["hometown"] == hometown
+        counter += 1
+      end 
+    end
+  end
+
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, season_array|
+    season_array.each do |contestant_information|
+      if contestant_information["hometown"] == hometown
+        return contestant_information["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  number_of_contestants = 0
+
+  data[season].each do |contestants|
+    contestants.each do |contestant_key, contestant_value|
+      if contestant_key == "age"
+        number_of_contestants += 1
+        age += contestant_value.to_f
+      end
+    end 
+  end
+  return (age / number_of_contestants).round
 end
